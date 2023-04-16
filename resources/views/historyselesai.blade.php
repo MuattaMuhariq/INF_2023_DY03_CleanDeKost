1ee67090ddc6d4bbdd3820303c072f2a2ce42d87
hadija humaira <hadija7@mhs.unsyiah.ac.id>
2023-04-16 15:09:26 +0700

    feat: modify file historyselesai

--- resources/views/historyselesai.blade.php
+++ resources/views/historyselesai.blade.php
@@ -12,7 +12,7 @@
 
     <div class="mt-6 md:flex md:items-center md:justify-between">
         <div class="inline-flex overflow-hidden bg-white border divide-x rounded-lg dark:bg-gray-900 rtl:flex-row-reverse dark:border-gray-700 dark:divide-gray-700">
-            <a href="/history"><button class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200  sm:text-sm dark:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
+            <a href="/historyproses"><button class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200  sm:text-sm dark:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
                 Diproses
             </button></a>
         
